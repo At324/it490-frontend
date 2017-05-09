85cfed87790a4a4b33770df2321283b31c09254e
galenlenardis <markegraf@gmail.com>
2017-05-09 16:58:32 -0400

    DMZ added

--- DMZ/testScrappingAPI.php
+++ DMZ/testScrappingAPI.php
@@ -1,12 +1,21 @@
 <?php
+	set_time_limit(0);
+
+	ignore_user_abort(true);
+
         $url = "https://www.newegg.com/Product/ProductList.aspx?Submit=ENE&N=-1&IsNodeId=1&Description=desktop%20graphics%20card&bop=And&PageSize=96&order=RATING";
         $html = file_get_contents($url);
-        if (preg_match("\item-container >/s(.*?)/s<item-operate >/", $html, $html_arr) === 0){
+      
+	if (preg_match_all('/<div class="item-container ">/(.*)\<div class ="item-operate ">/', $html, $html_arr))
+{
+
 //      $img = explode('src', $html_arr[1]);
 //      $img = explode("", $img[1]);
 //      $img = "https:".$img[1];
+
+//	echo $html;
         echo "found";
-        echo $html_arr[0];
+        print_r($html_arr);
 }
 else{
 
